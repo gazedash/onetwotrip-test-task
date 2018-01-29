6c3f2f2d8ef914cf88a796c5ec94b0a0cf5d20c4
Andrey <skrtroot@protonmail.com>
2018-01-29 15:05:35 +0300

    Render Widget in App, pass store to App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Provider from "./store/provider";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider story={<App />} />, document.getElementById('root'));
 registerServiceWorker();
