05605e890e9272a7b88b8d0fe12dae5e0e0762af
Andrey <skrtroot@protonmail.com>
2018-01-29 17:19:49 +0300

    Add styled-components

---
+++ src/components/StyledComponents.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+export const select = styled.select`
+  background: #f9f9f9;
+  border: 3px solid #eee;
+  min-width: 100px;
+  margin: 20px;
+  font-size: 16px;
+`;
+export const option = styled.option`
+  background: #f9f9f9;
+`;
+export const FlightRoot = styled.div`
+  padding: 15px 0px 15px 25px;
+  box-shadow: 0 5px 9px rgba(0, 0, 0, 0.2);
+`;
+export const FlightDest = styled.h5`
+  margin: 5px 0px;
+`;
+export const FlightCarrier = styled.h5`
+  margin: 10px 0px 0px 0px;
+`;
+export const FlightDate = styled.span`
+  font-weight: bold;
+  color: #6060ff;
+`;
+export const Title = styled.h2`
+  padding-left: 20px;
+  margin: 0;
+`;
