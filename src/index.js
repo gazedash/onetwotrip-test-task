2dc86d8cb03638740e9a2901fe588e6a4625816d
Andrey <skrtroot@protonmail.com>
2018-01-29 17:44:15 +0300

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import Provider from "./store/provider";
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Provider story={<App />} />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Provider story={<App />} />, document.getElementById("root"));
